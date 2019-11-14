c63ddf71e162d8310c5f9bf89b044f4a2c732848
Martina Rapp <rapp@fzi.de>
2019-11-14 14:40:22 +0100

    created common plugin with commonly shared constants

---
+++ bundles/org.palladiosimulator.analyzer.slingshot.common.constants/src/org/palladiosimulator/analyzer/slingshot/common/constants/model/ModelFileTypeConstants.java
@@ -0,0 +1,16 @@
+package org.palladiosimulator.analyzer.slingshot.common.constants.model;
+
+public class ModelFileTypeConstants {
+	
+	public static final String EMPTY_STRING = "";
+	
+	
+	public static final String ALLOCATION_FILE = "allocationFile";
+	public static final String USAGE_FILE = "usageFile";
+	public static final String MONITOR_REPOSITORY_FILE = "monitorRepositoryFile";
+
+	public static final String[] ALLOCATION_FILE_EXTENSION = new String[] { "*." + ModelTypeConstants.ALLOCATION_EXTENSION };
+	public static final String[] USAGEMODEL_FILE_EXTENSION = new String[] { "*." + ModelTypeConstants.USAGEMODEL_EXTENSION };
+	public static final String[] MONITOR_REPOSITORY_FILE_EXTENSION = new String[] { "*." + ModelTypeConstants.MONITOR_REPOSITORY_EXTENSION };
+
+}
