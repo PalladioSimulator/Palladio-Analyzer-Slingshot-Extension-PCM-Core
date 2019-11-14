2719767f6893af3e4a87781321f4867804bcd5a3
Martina Rapp <rapp@fzi.de>
2019-11-14 14:49:11 +0100

    workflow integration: added simulation launcher; updated dependencies in manifest

--- bundles/org.palladiosimulator.analyzer.slingshot.workflow/src/org/palladiosimulator/analyzer/slingshot/workflow/configuration/SimulationWorkflowConfiguration.java
+++ bundles/org.palladiosimulator.analyzer.slingshot.workflow/src/org/palladiosimulator/analyzer/slingshot/workflow/configuration/SimulationWorkflowConfiguration.java
@@ -3,10 +3,26 @@ package org.palladiosimulator.analyzer.slingshot.workflow.configuration;
 import org.palladiosimulator.analyzer.workflow.configurations.AbstractPCMWorkflowRunConfiguration;
 
 public class SimulationWorkflowConfiguration extends AbstractPCMWorkflowRunConfiguration {
+	
+	private ArchitecturalModelsConfiguration inputModels;
+
+	public SimulationWorkflowConfiguration(ArchitecturalModelsConfiguration architecturalModels) {
+		this.inputModels = architecturalModels;
+		
+		/**
+		 * workaround: 
+		 * allocation files are required by the parent class AbstractPCMWorkflowRunConfiguration.validateAndFreeze when loading PCMModels;
+		 * this existence check for PCM models should be done during configuration validation !!! needs refactoring
+		 * for simulation it is current not required; therefore pass empty list in order to successfully execute workflow
+		 * */
+		this.setUsageModelFile(inputModels.getUsageModelFile());
+		this.setAllocationFiles(inputModels.getAllocationFiles());
+	}
 
 	@Override
 	public String getErrorMessage() {
-		// TODO Auto-generated method stub
+		// configuration validation is already done in the LaunchConfiguration class
+		// currently no error messages available; return null otherwise workflow validation fails
 		return null;
 	}
 
--- bundles/org.palladiosimulator.analyzer.slingshot.workflow/src/org/palladiosimulator/analyzer/slingshot/workflow/launcher/SimulationLauncher.java
+++ bundles/org.palladiosimulator.analyzer.slingshot.workflow/src/org/palladiosimulator/analyzer/slingshot/workflow/launcher/SimulationLauncher.java
@@ -2,38 +2,69 @@ package org.palladiosimulator.analyzer.slingshot.workflow.launcher;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
+import org.palladiosimulator.analyzer.slingshot.workflow.api.SimulationWorkflowConfigurationConstants;
+import org.palladiosimulator.analyzer.slingshot.workflow.configuration.ArchitecturalModelsConfiguration;
+import org.palladiosimulator.analyzer.slingshot.workflow.configuration.SimulationWorkflowConfiguration;
+import org.palladiosimulator.analyzer.slingshot.workflow.launcher.jobs.SimulationRootJob;
 import org.palladiosimulator.analyzer.workflow.configurations.AbstractPCMLaunchConfigurationDelegate;
-import org.palladiosimulator.analyzer.workflow.configurations.AbstractPCMWorkflowRunConfiguration;
 
 import de.uka.ipd.sdq.workflow.jobs.IJob;
 import de.uka.ipd.sdq.workflow.logging.console.LoggerAppenderStruct;
 
-public class SimulationLauncher extends AbstractPCMLaunchConfigurationDelegate<AbstractPCMWorkflowRunConfiguration> {
+public class SimulationLauncher extends AbstractPCMLaunchConfigurationDelegate<SimulationWorkflowConfiguration> {
 	
 	private static final Logger LOGGER = Logger.getLogger(SimulationLauncher.class.getName());
 
 	@Override
-	protected AbstractPCMWorkflowRunConfiguration deriveConfiguration(ILaunchConfiguration configuration, String mode)
+	protected SimulationWorkflowConfiguration deriveConfiguration(ILaunchConfiguration configuration, String mode)
 			throws CoreException {
 		
 		LOGGER.info("SimulationLauncher.deriveConfiguration");
 		
 		// TODO Auto-generated method stub
-		return null;
+		return buildWorkflowConfiguration(configuration, mode);
 	}
 
 	
 	@Override
-	protected IJob createWorkflowJob(AbstractPCMWorkflowRunConfiguration config, ILaunch launch) throws CoreException {
-		LOGGER.info("SimulationLauncher.createWorkflowJob");
-		// TODO Auto-generated method stub
-		return null;
+	protected IJob createWorkflowJob(SimulationWorkflowConfiguration config, ILaunch launch) throws CoreException {
+		return new SimulationRootJob(config, launch);
+	}
+	
+	
+	private SimulationWorkflowConfiguration buildWorkflowConfiguration(ILaunchConfiguration configuration, String mode) {
+		
+		SimulationWorkflowConfiguration workflowConfiguration = null;
+		try {
+			Map<String, Object> launchConfigurationParams = configuration.getAttributes();
+			
+			if (LOGGER.isDebugEnabled()) {
+				for (Entry<String, Object> entry : launchConfigurationParams.entrySet()) {
+					LOGGER.debug(String.format("launch configuration param ['%s':'%s']", entry.getKey(), entry.getValue()));
+				}
+			}
+			
+			ArchitecturalModelsConfiguration architecturalModels = new ArchitecturalModelsConfiguration(
+					(String) launchConfigurationParams.get(SimulationWorkflowConfigurationConstants.USAGE_FILE)
+					, Arrays.asList((String) launchConfigurationParams.get(SimulationWorkflowConfigurationConstants.ALLOCATION_FILE))
+					, (String) launchConfigurationParams.get(SimulationWorkflowConfigurationConstants.MONITOR_REPOSITORY_FILE));
+			
+			workflowConfiguration = new SimulationWorkflowConfiguration(architecturalModels);
+			
+		} catch (CoreException e) {
+			LOGGER.error("Failed to read workflow configuration from passed launch configuration. Please check the provided launch configuration", e);
+		}
+		
+		return workflowConfiguration;
 	}
 	
     @Override
@@ -43,6 +74,8 @@ public class SimulationLauncher extends AbstractPCMLaunchConfigurationDelegate<A
         loggerList.add(setupLogger("org.palladiosimulator.analyzer.slingshot", logLevel, Level.DEBUG == logLevel ? DETAILED_LOG_PATTERN : SHORT_LOG_PATTERN));
         return loggerList;
     }
+
+
 
 
 }
