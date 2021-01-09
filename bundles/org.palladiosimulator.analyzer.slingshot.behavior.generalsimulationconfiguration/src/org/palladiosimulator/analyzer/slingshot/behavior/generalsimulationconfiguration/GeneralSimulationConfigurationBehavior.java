57cc109ce0acff848c4e6f4ccd5db6077cd63a5e
Julijan Katic <julijan@katic.io>
2021-01-09 18:41:50 +0100

    Created General Configuration bundle for configuring the simulators in advance.

---
+++ bundles/org.palladiosimulator.analyzer.slingshot.behavior.generalsimulationconfiguration/src/org/palladiosimulator/analyzer/slingshot/behavior/generalsimulationconfiguration/GeneralSimulationConfigurationBehavior.java
@@ -0,0 +1,41 @@
+package org.palladiosimulator.analyzer.slingshot.behavior.generalsimulationconfiguration;
+
+import de.uka.ipd.sdq.probfunction.math.IProbabilityFunctionFactory;
+import de.uka.ipd.sdq.probfunction.math.impl.ProbabilityFunctionFactoryImpl;
+import de.uka.ipd.sdq.simucomframework.variables.cache.StoExCache;
+
+import org.palladiosimulator.analyzer.slingshot.simulation.core.events.SimulationStarted;
+import org.palladiosimulator.analyzer.slingshot.simulation.extensions.behavioral.SimulationBehaviorExtension;
+import org.palladiosimulator.analyzer.slingshot.simulation.extensions.behavioral.annotations.OnEvent;
+import org.palladiosimulator.analyzer.slingshot.simulation.extensions.behavioral.results.ResultEvent;
+
+import org.apache.log4j.Logger;
+
+/**
+ * This behavior is used in order to have a general implementation for configuraring
+ * the simulator beforehand. For instance, this class initializes the {@link StoEx} cache
+ * on the simulation start.
+ * 
+ * @author Julijan Katic
+ * @version 1.0
+ */
+@OnEvent(when = SimulationStarted.class, then = {})
+public class GeneralSimulationConfigurationBehavior implements SimulationBehaviorExtension {
+	
+	private final static Logger LOGGER = Logger.getLogger(GeneralSimulationConfigurationBehavior.class);
+	
+	/**
+	 * Initializes the simulation for each component.
+	 * 
+	 * @return an empty set.
+	 */
+	public ResultEvent<?> onSimulationStarted(final SimulationStarted simulationStarted) {
+		/* Initialize ProbFunction and StoExCache, otherwise StackContext won't work */
+		final IProbabilityFunctionFactory probabilityFunctionFactory = ProbabilityFunctionFactoryImpl.getInstance();
+		StoExCache.initialiseStoExCache(probabilityFunctionFactory);
+		LOGGER.info("Initialized probability function");
+		
+		return ResultEvent.empty();
+	}
+	
+}
